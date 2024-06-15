27deafccd8e573100b892d9772eb44f2149f44d2
JazzerThighs <profenjb199@gmail.com>
2024-06-14 23:44:06 -0400

    modified:   src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -68,8 +68,8 @@ impl QopEdit {
             }
         }
     }
-    pub fn kcs_swap_labels(&mut self, kc1: KeyCode, kc2: KeyCode) -> Option<(usize, usize)> {
-        // swaps only the labels for 2 existing keys, which leaves alone all of the nodes' usize values in the rest of the QopEdit.
+    pub fn kcs_swap_labels(&mut self, kc1: KeyCode, kc2: KeyCode) {
+        // swaps only the keycode labels for 2 existing keys, which leaves alone all of the nodes' usize values in the rest of the QopEdit.
         let (mut i1, mut i2): (Option<usize>, Option<usize>) = (None, None);
         for i in 0..self.key_codes.len() {
             if self.key_codes[i] == kc1 {
@@ -81,20 +81,25 @@ impl QopEdit {
         if i1.is_some() && i2.is_some() {
             (self.key_codes[i1.unwrap()], self.key_codes[i2.unwrap()]) =
                 (self.key_codes[i2.unwrap()], self.key_codes[i1.unwrap()]);
-            return Some((i1.unwrap(), i2.unwrap()));
         }
-        return None;
     }
     pub fn kcs_swap_idxs(&mut self, kc1: KeyCode, kc2: KeyCode) {
-        // swaps the position of 2 existing keys, and in turn, would then need to swap all of the nodes' usize values in all of the plucks' and sets' various fields.
-        let res: Option<(usize, usize)> = self.kcs_swap_labels(kc1, kc2);
-        if let Some((i1, i2)) = res {
+        // swaps the nodes of 2 existing keys in all of the plucks' and sets' various fields.
+        let (mut i1, mut i2): (Option<usize>, Option<usize>) = (None, None);
+        for i in 0..self.key_codes.len() {
+            if self.key_codes[i] == kc1 {
+                i1 = Some(i);
+            } else if self.key_codes[i] == kc2 {
+                i2 = Some(i);
+            }
+        }
+        if i1.is_some() && i2.is_some() {
             let k_idxs_swap = |k_idx_vec: &mut Vec<usize>| {
                 k_idx_vec.iter_mut().for_each(|k: &mut usize| {
-                    if *k == i1 {
-                        *k = i2;
-                    } else if *k == i2 {
-                        *k = i1;
+                    if *k == i1.unwrap() {
+                        *k = i2.unwrap();
+                    } else if *k == i2.unwrap() {
+                        *k = i1.unwrap();
                     }
                 });
             };
