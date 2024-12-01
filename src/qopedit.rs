7cc9e71ab4fd022935afdd1b001b69946acdc550
JazzerThighs <profenjb199@gmail.com>
2024-12-01 18:51:53 -0500

    modified:   src/qopedit.rs

--- src/qopedit.rs
+++ src/qopedit.rs
@@ -1,6 +1,6 @@
-pub mod qe_asserts;
-pub mod qe_gut_methods;
-pub mod qe_kcs_methods;
-pub mod qe_misc_btns;
-pub mod qe_set_methods;
-pub mod qe_trnsp_methods;
+mod qe_asserts;
+mod qe_gut_methods;
+mod qe_kcs_methods;
+mod qe_misc_btns;
+mod qe_set_methods;
+mod qe_trnsp_methods;
